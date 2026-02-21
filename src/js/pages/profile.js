b84bb1d4f79980dafa1bd1afd22b598e8f1bc309
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-02-21 20:01:51 +0000

    profile loader was updated

--- src/js/pages/profile.js
+++ src/js/pages/profile.js
@@ -1,13 +1,36 @@
 import ApiRequest from "../utils/ApiRequest.js";
 import {Template} from "../config.js";
 import Mustache from "./../utils/mustache.js"
+import {getFormData} from "../utils/helpers.js";
 
 export default async function () {
+
     const res = await ApiRequest.getProfile()
+
     if (!res) {
         return
     }
-    console.log('loading from profile.js', res)
-    $('#dataCard').append(Mustache.render(Template.component.dummyProfileTemplate(), res))
 
+    $('#dummyProfile').replaceWith(Mustache.render(Template.component.dummyProfileTemplate(), res))
+
+
+    $('#updateProfileForm').submit(async function (e) {
+        e.preventDefault();
+
+        // Parse form
+        const data = getFormData(e.target)
+
+        // Upload
+        let uploadRes = undefined
+        if (data?.avatar?.name)
+            uploadRes = await ApiRequest.uploadFile(data.avatar)
+
+        if (uploadRes?.publicUrl) {
+            $('#userAvatar').attr('src', uploadRes.publicUrl)
+        }
+        ApiRequest.updateProfile({
+            ...data,
+            avatarURL: uploadRes?.key || uploadRes?.key === null ? uploadRes.key : undefined
+        })
+    });
 }
